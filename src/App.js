230b4f821bd923b5747382f50bf12407073e5a50
Ryan student <s4581368@student.uq.edu.au>
2021-08-11 13:08:43 +1000

    double commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {COWTRIAL_ABI, COWTRIAL_ADDRESS} from './config'
+import Web3 from 'web3'
+import './App.css'
+import detectEthereumProvider from '@metamask/detect-provider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const provider = await detectEthereumProvider();
+    //console.log(provider)
+    const web3 = new Web3(provider);
+    let getAccount = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    this.setState({account: getAccount[0]});
+
+    const CowContract = new web3.eth.Contract(COWTRIAL_ABI, COWTRIAL_ADDRESS);
+    
+    this.setState({CowContract});
+    CowContract.methods.TokenIDToOwner(1).call(function (err, res) {
+      if (err) {
+        console.log("An Error has occured", err);
+        return
+      }
+      console.log("The Owner is", res);
+    })
+  }
+    
+
+    //console.log(CowContract);
+
+
+
+  
+
+
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '' }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello, World!</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
