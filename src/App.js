afa40861d1e913dfd17d6eee1bfa1f33f49d8548
Ryan student <s4581368@student.uq.edu.au>
2021-08-11 18:02:22 +1000

    Progress, next thing to do is implement NFT minting

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import {COWTRIAL_ABI, COWTRIAL_ADDRESS} from './config'
 import Web3 from 'web3'
 import './App.css'
 import detectEthereumProvider from '@metamask/detect-provider';
+import { thistle } from 'color-name';
 
 
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '' }
+    
+    this.state = {value: ''}
+    this.state = {attributeValue: ''}
+    this.state = {ownerAccount: ''}
+    this.state = {Attributes: []}
+    this.state = {mintNLIS: ''}
+    this.state = {mintPIC: ''}
+    this.state = {mintDNAHASH: ''}
+    this.state = {mintADDRESS: ''}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
   componentWillMount() {
     this.loadBlockchainData()
+    document.body.style.backgroundColor = "darkgrey"
   }
 
   async loadBlockchainData() {
@@ -18,41 +38,168 @@ class App extends Component {
     const web3 = new Web3(provider);
     let getAccount = await window.ethereum.request({ method: 'eth_requestAccounts' });
     this.setState({account: getAccount[0]});
+    //console.log(this.state.account)
 
     const CowContract = new web3.eth.Contract(COWTRIAL_ABI, COWTRIAL_ADDRESS);
     
-    this.setState({CowContract});
-    CowContract.methods.TokenIDToOwner(1).call(function (err, res) {
+    this.setState({contract: CowContract});
+    
+  }
+    
+  async getTokenOwner(tokenID) {
+    //console.log(this.state)
+    let owner;
+    if (tokenID == "") {
+      return 0
+    }
+    owner = await this.state.contract.methods.TokenIDToOwner(tokenID).call(function (err, res) {
+    if (err) {
+      console.log("An Error has occured", err);
+      return
+      }
+      console.log("The Owner is", res);
+      //owner = res
+    })
+
+    //console.log(owner)
+    return owner
+
+  }
+
+  async handleChange(event) {
+    let owner;
+    try {
+      owner = await this.getTokenOwner(parseInt(event.target.value))
+    } catch (err) {
+      console.log(err)
+    }
+    console.log(owner)
+    this.setState({ownerAccount: owner})
+  }
+
+  async handleAttributes(event) {
+    
+    let attributes;
+    try {
+      attributes = await this.getAttributes(parseInt(event.target.value))
+    } catch (err) {
+      console.log(err)
+    }
+    this.setState({NLIS: attributes[0]})
+    this.setState({PIC: attributes[1]})
+    this.setState({DNAHASH: attributes[2]})
+    this.setState({OWNER: attributes[3]})
+    
+    
+
+  }
+
+  handleSubmit(event) {
+    //alert("The owner is " + this.state.ownerAccount);
+    event.preventDefault()
+  }
+
+  async getAttributes(tokenID) {
+    let attributes;
+    if (tokenID == "") {
+      return 0;
+    }
+
+    const DNAHash = await this.state.contract.methods.TokenIDToDNAHash(tokenID).call(function (err, res) {
       if (err) {
         console.log("An Error has occured", err);
         return
-      }
-      console.log("The Owner is", res);
-    })
-  }
+        }
+        console.log("The DNAHash is", res);
+        //owner = res
+      })
     
 
-    //console.log(CowContract);
 
+    attributes = await this.state.contract.methods.DNAHashToAttributes(DNAHash).call(function (err, res) {
+      if (err) {
+        console.log("An Error has occured", err);
+        return
+        }
+        console.log("The Owner is", res);
+        //owner = res
+      })
+      
+      return attributes
+  }
 
+  handleCreation(){
+    alert(this.state.mintNLIS)
+  }
 
-  
-
-
-
-  constructor(props) {
-    super(props)
-    this.state = { account: '' }
+  mintChangeHandler(event) {
+    
   }
 
   render() {
     return (
       <div className="container">
-        <h1>Hello, World!</h1>
-        <p>Your account: {this.state.account}</p>
+        
+        <h1>Centric Trial Contract</h1>
+        <p>Your MetaMask account: {this.state.account}</p>
+        <div className="getOwners">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              TokenID:
+              <input type="text" value={this.state.value} onChange={this.handleChange}/>
+            </label>
+            <input type="submit" value="Submit"/>
+          </form>
+          <p>Token Owner: {this.state.ownerAccount}</p>
+
+        </div>
+        <div className="getOwners">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Get Attributes from tokenID:
+              <input type="text" value={this.state.attributeValue} onChange={this.handleAttributes.bind(this)}/>
+            </label>
+            <input type="submit" value="Submit"/>
+          </form>
+          <p>ATTRIBUTES</p>
+          <p>NLIS: {this.state.NLIS} </p>
+          <p>PIC: {this.state.PIC}</p>
+          <p>DNAHASH: {this.state.DNAHASH}</p>
+          <p>OWNER: {this.state.OWNER}</p>
+        </div>
+        <div>
+          <h3>MINT TOKEN</h3>
+          <form onSubmit={this.handleCreation.bind(this)}>
+            <label>
+              NLIS:  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              <input type="text" value={this.state.mintNLIS} onChange={this.doNothing}/>
+            </label>
+            <br></br>
+            <label>
+              PIC: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
+              <input type="text" value={this.state.mintPIC} onChange={this.doNothing}/>
+            </label>
+            <br></br>
+            <label>
+              DNAHASH:
+              <input type="text" value={this.state.mintDNAHASH} onChange={this.doNothing}/>
+            </label>
+            <br></br>
+            <label>
+              OWNER: &nbsp; &nbsp;&nbsp;
+              <input type="text" value={this.state.mintADDRESS} onChange={this.doNothing}/>
+            </label>
+            <br></br>
+            <input type="submit" value="MINT"/>
+          </form>
+
+        </div>
+
+
       </div>
     );
+    }
   }
-}
+
 
 export default App;
